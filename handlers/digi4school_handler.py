80d34393c686f81c8ffc9befbd8a701b81f5d6c2
pfeiff190100 <pfeiff190100@sr.htlweiz.at>
2023-03-24 13:56:55 +0100

    working on book download

--- handlers/digi4school_handler.py
+++ handlers/digi4school_handler.py
@@ -1,8 +1,11 @@
-import requests
-from requests_html import HTMLSession
-from handlers.config_handler import Config
-from bs4 import BeautifulSoup as bs
 import os
+
+import requests
+from bs4 import BeautifulSoup as bs
+from requests.utils import cookiejar_from_dict
+
+from handlers.config_handler import Config
+
 
 class Digi4school:
     def __init__(self):
@@ -54,24 +57,47 @@ class Digi4school:
     def download_book(self, book_id):
         url = self.book_url + book_id
         down_dir = f'download/{book_id}'
+
+        cookies_dict = requests.utils.dict_from_cookiejar(self.session.cookies)
+        cookies = requests.utils.cookiejar_from_dict(cookies_dict)
+
+        
+        url = "https://a.digi4school.at/ebook/7553/"
+
+        response = requests.get(url, cookies=self.session.cookies ,allow_redirects=True)
+
+        initiator_chain = []
+
+        for redirect in response.history:
+            initiator_chain.append(redirect.url)
+
+        initiator_chain.append(response.url)
+
+        print(initiator_chain)
+
+        digi4s = cookies.get("digi4s")
+        ad_session_id = cookies.get("ad_session_id")
+        print(ad_session_id)
+        print(digi4s)
+
+        headers = {
+            'Cookie': 'digi4b="2296329404%2c7553%2c22s5bzuagyky%2c252212%20{261%201679642398%20AC09555B659FA11FB163DACED7E711E8A4426B7F}"; ad_session_id="%s"; digi4s="%s"' % (ad_session_id, digi4s),
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+        }
         counter = 1
 
         os.makedirs(down_dir)
 
-        session = HTMLSession()
-        response = session.get(url)
 
         while True:
             file_url = f"{url}/{counter}.svg"
-            response = session.get(file_url, cookies=self.session.cookies)
+            response = requests.get(file_url, cookies=self.session.cookies, headers=headers, timeout=10)
 
-            # Save the SVG file
-            with open(f"{down_dir}/{counter}.svg", "wb") as f:
-                f.write(response.content)
-
-            # Exit loop if the file doesn't exist
-            if "404 - Not Found" in response.text:
+            if response.status_code == 400:
                 break
+
+            with open(f"{down_dir}/{counter}.svg", "w+", encoding="utf8") as f:
+                f.write(response.text)
 
             counter += 1
 
