6f6d05c6512a879758e5caba14ff790625b6a933
Pfefan <stefanpfeiffer04@gmail.com>
2024-03-12 21:09:12 +0100

    Restructor auth test for better overview

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -8,7 +8,7 @@ from src.handlers.authentication import AuthAndTokenHandler
 from src.handlers.book_fetcher import BookDataRetriever
 
 
-class TestAUTH:
+class TestLogin:
     @classmethod
     def setup_class(cls):
         load_dotenv()
@@ -19,6 +19,24 @@ class TestAUTH:
         login_status, updated_session = auth.login_user(session)
         assert login_status is True
         assert updated_session is session
+
+    def test_login_user_failure(self):
+        session = requests.Session()
+        auth = AuthAndTokenHandler()
+        original_email = os.environ['EMAIL']
+        original_password = os.environ['PASSWORD']
+        os.environ['EMAIL'] = "WrongEmail"
+        os.environ['PASSWORD'] = "WrongPassword"
+        login_status, updated_session = auth.login_user(session)
+        assert login_status is False
+        assert updated_session is session
+        os.environ['EMAIL'] = original_email
+        os.environ['PASSWORD'] = original_password
+
+class TestTokenProcessing:
+    @classmethod
+    def setup_class(cls):
+        load_dotenv()
 
     def test_token_processing(self):
         session = requests.Session()
@@ -40,12 +58,3 @@ class TestAUTH:
         data = [book for book in data if book[2] == 'English Unlimited HTL 4/5, Schülerbuch mit Audio-CD und CD-ROM und E-Book'][0]
         url = auth.token_processing(data, session)
         assert url == 'https://a.digi4school.at/ebook/' + data[0] + '/1001'
-
-    def test_login_user_failure(self):
-        session = requests.Session()
-        auth = AuthAndTokenHandler()
-        os.environ['EMAIL'] = "WrongEmail"
-        os.environ['PASSWORD'] = "WrongPassword"
-        login_status, updated_session = auth.login_user(session)
-        assert login_status is False
-        assert updated_session is session
